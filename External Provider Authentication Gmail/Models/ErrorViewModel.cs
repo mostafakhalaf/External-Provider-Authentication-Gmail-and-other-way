fd7f864920b7354eba8b4530c2f67f9d91ca7154
mostafa khalaf <mostafakhalafmohamed@gmail.com>
2022-06-15 14:52:32 +0200

    Add project files.

---
+++ External Provider Authentication Gmail/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace External_Provider_Authentication_Gmail.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
